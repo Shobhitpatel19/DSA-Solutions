630053110dc9e268114864b1bae88578bdca4bbd
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-03-06 20:09:23 +0530

    Time: 12 ms (86.16%), Space: 44.7 MB (86.73%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,28 +1,35 @@
 class Solution {
+    public boolean canEatBananas(int[] piles, int mid, int hour){
+        int time = 0;
+        for(int val: piles){
+            time += val/mid;
+            if(val%mid != 0){
+                time++;
+            }
+        }
+        return time <= hour;
+    }
     public int minEatingSpeed(int[] piles, int h) {
-        int las = 1;
-        int has = 0;
-        for(int pile: piles){
-            has = Math.max(pile, has);
-        }
-        
-        while(las < has){
-            int as = (las + has) / 2;
-            
-            // calculate hours
-            int totalTime = 0;
-            for(int pile: piles){
-                int time = (pile + as - 1) / as;
-                totalTime += time;
-            }
-            
-            if(totalTime > h){
-                las = as + 1;
-            } else {
-                has = as;
+        // low and high are the count of bananas per hour.
+        int low = 1;
+        int high = 0;
+
+        // This loop is setting high = maximum value in piles
+        for(int val: piles){
+            if(val > high){
+                high = val;
             }
         }
-        
-        return las;
+
+        while(low < high){
+            int mid = low + (high-low)/2;
+
+            if(canEatBananas(piles, mid, h)){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return low;
     }
 }
