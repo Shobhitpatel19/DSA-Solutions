a772943f25ef7b5f23ade8c5cf0c5754fc438670
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-04-01 16:08:43 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (45.57%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,17 +1,18 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        /*
-          \\s: This represents any whitespace character.
-          +: This quantifier matches one or more occurrences of the preceding element, which in this case is whitespace characters.
-          $: This asserts the position at the end of the input.
-        */
-        // Trim trailing spaces
-        s = s.replaceAll("\\s+$", "");
+        int n = s.length();
+        if (n == 0)
+            return 0;
+        int i = n - 1;
+        while (i >= 0 && s.charAt(i) == ' ')
+            i--;
 
-        // Find the last space character
-        int lastSpace = s.lastIndexOf(' ');
-
-        // Calculate the length of the last word
-        return lastSpace == -1 ? s.length() : s.length() - lastSpace - 1;
+        int length = 0;
+        for (; i >= 0; i--) {
+            if (s.charAt(i) == ' ')
+                break;
+            length++;
+        }
+        return length;
     }
 }
