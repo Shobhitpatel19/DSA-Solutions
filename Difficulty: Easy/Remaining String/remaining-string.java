44be77e448209436ef471e037a0bbbd5364cfdcd
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-07-16 16:36:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remaining String/remaining-string.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            String s = sc.next();
+            char ch = sc.next().charAt(0);
+            int count = sc.nextInt();
+            Solution ob = new Solution();
+            String result = ob.printString(s, ch, count);
+
+            System.out.println(result);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public String printString(String s, char ch, int count) {
+        // code here
+        int i=0;
+        int n=0;
+        while(i<s.length() && n<count){
+            if(s.charAt(i) == ch){
+                n++;
+            }
+            i++;
+        }
+        return s.substring(i, s.length());
+    }
+}
