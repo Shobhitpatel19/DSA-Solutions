2fe9b1a732ddf2ae23604f2c2c251bf23ba77c68
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-08-02 06:38:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -0,0 +1,75 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String s1 = br.readLine();
+            String[] S = s1.split(" ");
+            String s = S[0];
+            String t = S[1];
+            Solution ob = new Solution();
+            int ans = ob.editDistance(s, t);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int editDistance(String str1, String str2) {
+        // Code here
+        int m = str1.length();
+        int n = str2.length();
+        
+        // Create a table to store results of subproblems
+        int dp[][] = new int[m + 1][n + 1];
+
+        // Fill d[][] in bottom up manner
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                // If first string is empty, only option is
+                // to insert all characters of second string
+                if (i == 0)
+                    dp[i][j] = j; // Min. operations = j
+
+                // If second string is empty, only option is
+                // to remove all characters of second string
+                else if (j == 0)
+                    dp[i][j] = i; // Min. operations = i
+
+                // If last characters are same, ignore last
+                // char and recur for remaining string
+                else if (str1.charAt(i - 1)
+                         == str2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+
+                // If the last character is different,
+                // consider all possibilities and find the
+                // minimum
+                else
+                    dp[i][j]
+                        = 1
+                          + min(
+                              dp[i][j - 1], // Insert
+                              dp[i - 1][j], // Remove
+                              dp[i - 1][j - 1]); // Replace
+            }
+        }
+
+        return dp[m][n];
+    
+    }
+
+    
+    
+    static int min(int x, int y, int z)  {
+        return Math.min(x, Math.min(y, z));
+    }
+}
