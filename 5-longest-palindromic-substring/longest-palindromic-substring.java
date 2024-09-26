e3a4019483b18d892fe5b8c47514e049cebc0e12
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-09-26 18:46:33 +0530

    Time: 150 ms (35.06%) | Memory: 46.8 MB (8.36%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,28 +1,35 @@
-public class Solution {
+class Solution {
     public String longestPalindrome(String s) {
-        if (s.length() <= 1) {
-            return s;
-        }
-
-        int maxLen = 1;
         int start = 0;
         int end = 0;
-        boolean[][] dp = new boolean[s.length()][s.length()];
+        int n = s.length();
 
-        for (int i = 0; i < s.length(); ++i) {
-            dp[i][i] = true;
-            for (int j = 0; j < i; ++j) {
-                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
-                    dp[j][i] = true;
-                    if (i - j + 1 > maxLen) {
-                        maxLen = i - j + 1;
-                        start = j;
-                        end = i;
+        boolean[][] dp = new boolean[n][n];
+        for(int g=0; g<s.length(); g++){
+            for(int i=0, j=g; j<dp.length; i++,j++){
+                if(g==0){
+                    dp[i][j] = true;
+                    start = i;
+                    end = j;
+                }else if(g==1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                        start = i;
+                        end = j;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]){
+                        dp[i][j] = true;
+                        start = i;
+                        end = j;
+                    }else{
+                        dp[i][j] = false;
                     }
                 }
             }
         }
-
-        return s.substring(start, end + 1);
+        return s.substring(start, end+1);
     }
 }
