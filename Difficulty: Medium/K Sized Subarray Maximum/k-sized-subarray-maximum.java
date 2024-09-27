5efc3390c43ff570b76ca8c12a3575cbc975c72b
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-09-27 13:24:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.java
@@ -0,0 +1,109 @@
+//{ Driver Code Starts
+// Initial template for JAVA
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        // taking input using class Scanner
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // taking total number of elements
+            int k = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] tokens = line.split(" ");
+
+            // Create an ArrayList to store the integers
+            ArrayList<Integer> array = new ArrayList<>();
+
+            // Parse the tokens into integers and add to the array
+            for (String token : tokens) {
+                array.add(Integer.parseInt(token));
+            }
+
+            int[] arr = new int[array.size()];
+            int idx = 0;
+            for (int i : array) arr[idx++] = i;
+            ArrayList<Integer> res = new Solution().max_of_subarrays(k, arr);
+
+            // printing the elements of the ArrayList
+            for (int i = 0; i < res.size(); i++) System.out.print(res.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for JAVA
+
+class Solution {
+    // Function to find maximum of each subarray of size k.
+    public ArrayList<Integer> max_of_subarrays(int K, int arr[]) {
+        // Your code here
+        // Create a Double Ended Queue, Qi
+        // that will store indexes of array elements
+        // The queue will store indexes of
+        // useful elements in every window and it will
+        // maintain decreasing order of values
+        // from front to rear in Qi, i.e.,
+        // arr[Qi.front[]] to arr[Qi.rear()]
+        // are sorted in decreasing order
+        Deque<Integer> Qi = new LinkedList<Integer>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        int N = arr.length;
+
+        /* Process first k (or first window)
+        elements of array */
+        int i;
+        for (i = 0; i < K; ++i) {
+
+            // For every element, the previous
+            // smaller elements are useless so
+            // remove them from Qi
+            while (!Qi.isEmpty()
+                   && arr[i] >= arr[Qi.peekLast()])
+
+                // Remove from rear
+                Qi.removeLast();
+
+            // Add new element at rear of queue
+            Qi.addLast(i);
+        }
+
+        // Process rest of the elements,
+        // i.e., from arr[k] to arr[n-1]
+        for (; i < N; ++i) {
+
+            // The element at the front of the
+            // queue is the largest element of
+            // previous window, so print it
+            //System.out.print( + " ");
+            ans.add(arr[Qi.peek()]);
+            // Remove the elements which
+            // are out of this window
+            while ((!Qi.isEmpty()) && Qi.peek() <= i - K)
+                Qi.removeFirst();
+
+            // Remove all elements smaller
+            // than the currently
+            // being added element (remove
+            // useless elements)
+            while ((!Qi.isEmpty())
+                   && arr[i] >= arr[Qi.peekLast()])
+                Qi.removeLast();
+
+            // Add current element at the rear of Qi
+            Qi.addLast(i);
+        }
+
+        // Print the maximum element of last window
+        // System.out.print(arr[Qi.peek()]);
+        ans.add(arr[Qi.peek()]);
+        return ans;
+    }
+}
