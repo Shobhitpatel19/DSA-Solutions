322ffcd733d0000251edf0fc41a11e3604520c38
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-10-04 18:11:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Combination Sum/combination-sum.java
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Driverclass
+{
+    static ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+    public static void main (String[] args)throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(System.out);
+        int t = Integer.parseInt(br.readLine());
+        
+        while(t-- >0)
+        {
+            String str[] = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            ArrayList<Integer> list = new ArrayList<>();
+            str = br.readLine().trim().split(" ");
+            for(int i = 0; i <n ;i++)
+                list.add(Integer.parseInt(str[i]));
+            str = br.readLine().trim().split(" ");    
+            int sum = Integer.parseInt(str[0]);
+            
+            Solution ob = new Solution();
+            
+            res = ob.combinationSum(list, sum);
+            if (res.size() == 0) {
+    			out.print("Empty");
+    		}
+ 
+    		// Print all combinations stored in res.
+    		for (int i = 0; i < res.size(); i++) {
+    			if (res.size() > 0) {
+    				out.print("(");
+    				List<Integer> ij = res.get(i);
+    				for (int j = 0; j < ij.size(); j++) {
+    				    
+    					out.print(ij.get(j));
+    					if(j < ij.size()-1)
+    					 out.print(" ");
+    				}
+    				out.print(")");
+    			}
+    		}
+    		out.println();
+    		res.clear();
+	    }
+	    out.flush();
+    }
+    
+}
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    //Function to return a list of indexes denoting the required 
+    //combinations whose sum is equal to given number.
+    
+    
+    static ArrayList<ArrayList<Integer>> 
+    combinationSum(ArrayList<Integer> arr, int sum)
+    {
+        // add your code here
+        ArrayList<ArrayList<Integer> > ans
+            = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        // first do hashing since hashset does not always
+        // sort
+        //  removing the duplicates using HashSet and
+        // Sorting the arrayList
+
+        Set<Integer> set = new HashSet<>(arr);
+        arr.clear();
+        arr.addAll(set);
+        Collections.sort(arr);
+
+        findNumbers(ans, arr, sum, 0, temp);
+        return ans;
+    }
+    
+    static void findNumbers(ArrayList< ArrayList<Integer> > ans,
+                ArrayList<Integer> arr, int sum, int index,
+                ArrayList<Integer> temp)
+    {
+
+        if (sum == 0) {
+
+            // Adding deep copy of list to ans
+
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for (int i = index; i < arr.size(); i++) {
+
+            // checking that sum does not become negative
+
+            if ((sum - arr.get(i)) >= 0) {
+
+                // adding element which can contribute to
+                // sum
+
+                temp.add(arr.get(i));
+
+                findNumbers(ans, arr, sum - arr.get(i), i,
+                            temp);
+
+                // removing element from list (backtracking)
+                temp.remove(arr.get(i));
+            }
+        }
+    }
+}
