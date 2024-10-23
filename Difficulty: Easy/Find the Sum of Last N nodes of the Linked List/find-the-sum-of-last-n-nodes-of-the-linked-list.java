0c10812694b7e6d3c5b7de58b0c601b8c3344b45
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-10-23 14:28:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.java
@@ -0,0 +1,89 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+Node class is as follows:
+
+class Node {
+    int data;
+    Node next;
+
+    public Node (int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+*/
+
+class Solution {
+
+    // Return the sum of last k nodes
+    public int sumOfLastN_Nodes(Node head, int n) {
+        // write code here
+        if(head == null){
+            return 0;
+        }
+        
+        int size = 1;
+        Node temp = head;
+        while(temp.next != null){
+            size++;
+            temp = temp.next;
+        }
+        
+        size = size - n;
+        temp = head;
+        int k = 1;
+        int sum = 0;
+        while(temp != null){
+            if(k > size){
+                sum += temp.data;
+            }
+            k++;
+            temp = temp.next;
+        }
+        return sum;
+    }
+}
+
+//{ Driver Code Starts.
+
+class LinkedList {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            ArrayList<Integer> arr = new ArrayList<>();
+            String input = br.readLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            int n = Integer.parseInt(br.readLine());
+            Node head = new Node(arr.get(0));
+            Node tail = head;
+            for (int i = 1; i < arr.size(); ++i) {
+                tail.next = new Node(arr.get(i));
+                tail = tail.next;
+            }
+            Solution ob = new Solution();
+            System.out.println(ob.sumOfLastN_Nodes(head, n));
+        }
+    }
+}
+
+// } Driver Code Ends
