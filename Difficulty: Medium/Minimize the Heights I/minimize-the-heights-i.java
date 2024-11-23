0bd92304e02526832069a7efcdd90f83ad91defa
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-11-23 10:45:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize the Heights I/minimize-the-heights-i.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // Read the number of test cases (t)
+        int t = Integer.parseInt(br.readLine().trim());
+
+        while (t-- > 0) {
+            int k = Integer.parseInt(br.readLine().trim());
+            String input = br.readLine().trim();
+
+            // Split the input line into integers and store them in the array
+            String[] tokens = input.split(" ");
+            int[] arr = new int[tokens.length];
+            for (int i = 0; i < tokens.length; i++) {
+                arr[i] = Integer.parseInt(tokens[i]);
+            }
+
+            // Create an instance of the Solution class
+            Solution ob = new Solution();
+
+            // Call the getMinDiff method
+            int res = ob.getMinDiff(k, arr);
+
+            // Print the result
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    public int getMinDiff(int k, int[] arr) {
+        // code here
+        int n=arr.length;
+        if(n==1) return 0;
+        Arrays.sort(arr);
+        int ans=arr[n-1]-arr[0];
+        for(int i=0;i<n-1;i++){
+            int mn=Math.min(arr[i+1]-k,arr[0]+k);
+            int mx=Math.max(arr[n-1]-k,arr[i]+k);
+            ans=Math.min(ans,Math.abs(mx-mn));
+        }
+        return ans;
+    }
+}
