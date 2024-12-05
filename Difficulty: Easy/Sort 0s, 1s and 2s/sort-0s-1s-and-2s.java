7cb624b3c5497f4846084e26ef274afb0f99afb6
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2024-12-05 13:22:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
+++ Difficulty: Easy/Sort 0s, 1s and 2s/sort-0s-1s-and-2s.java
@@ -11,11 +11,9 @@ class GFG {
         while (t-- > 0) {
             String input = br.readLine();
             String[] inputArray = input.split("\\s+");
-            ArrayList<Integer> a = new ArrayList<>();
+            int a[] = new int[inputArray.length];
 
-            for (String s : inputArray) {
-                a.add(Integer.parseInt(s));
-            }
+            for (int i = 0; i < a.length; i++) a[i] = Integer.parseInt(inputArray[i]);
 
             Solution ob = new Solution();
             ob.sort012(a);
@@ -24,33 +22,54 @@ class GFG {
                 System.out.print(num + " ");
             }
             System.out.println();
+            System.out.println("~");
         }
     }
 }
+
 
 // } Driver Code Ends
 
 
 class Solution {
     // Function to sort an array of 0s, 1s, and 2s
-    public void sort012(ArrayList<Integer> arr) {
-        // code here
-        int low = 0, mid = 0, high = arr.size() - 1;
-
-        // Dutch National Flag Algorithm
+    public void sort012(int[] arr) {
+        int low = 0;       // pointer for 0s
+        int mid = 0;       // current element being examined
+        int high = arr.length - 1;  // pointer for 2s
+        
         while (mid <= high) {
-            if (arr.get(mid) == 0) {
-                // Swap arr[low] and arr[mid]
-                Collections.swap(arr, low, mid);
-                low++;
-                mid++;
-            } else if (arr.get(mid) == 1) {
-                mid++;  // Leave 1 in place, just move to the next element
-            } else {
-                // Swap arr[mid] and arr[high]
-                Collections.swap(arr, mid, high);
-                high--;
+            switch (arr[mid]) {
+                case 0:
+                    // Swap 0 to the low section
+                    swap(arr, low, mid);
+                    low++;
+                    mid++;
+                    break;
+                    
+                case 1:
+                    // 1 is in the right place, just move mid
+                    mid++;
+                    break;
+                    
+                case 2:
+                    // Swap 2 to the high section
+                    swap(arr, mid, high);
+                    high--;
+                    break;
             }
         }
     }
+    
+    // Helper method to swap elements in the array
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
+
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
