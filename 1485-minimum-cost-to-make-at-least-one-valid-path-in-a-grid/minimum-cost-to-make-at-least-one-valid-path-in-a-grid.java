4145605c66b560f608ec993e9f1ee9cc97256a6c
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2025-01-18 23:46:26 +0530

    Time: 14 ms (75.91%) | Memory: 45.1 MB (56.20%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,32 +1,42 @@
 class Solution {
     public int minCost(int[][] grid) {
-        int m = grid.length, n = grid[0].length;
-        int[][] dist = new int[m][n];
-        for (int[] row : dist) Arrays.fill(row, Integer.MAX_VALUE);
-        Deque<int[]> dq = new ArrayDeque<>();
-        dq.offerFirst(new int[]{0, 0});
-        dist[0][0] = 0;
-        int[] dx = {0, 0, 1, -1}, dy = {1, -1, 0, 0};
+        int m=grid.length;
+        int n=grid[0].length;
+
+        int[][] minCost=new int[m][n];
+        for(int i=0; i<m; i++){
+            Arrays.fill(minCost[i], Integer.MAX_VALUE);
+        }
+
+        Deque<int[]> dque=new ArrayDeque<>();
+        dque.offerFirst(new int[]{0,0});
+        minCost[0][0]=0;
         
-        while (!dq.isEmpty()) {
-            int[] curr = dq.pollFirst();
-            int x = curr[0], y = curr[1];
-            int curDir = grid[x][y] - 1;
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = x + dx[dir], ny = y + dy[dir];
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n) {
-                    int cost = dist[x][y] + (dir == curDir ? 0 : 1);
-                    if (cost < dist[nx][ny]) {
-                        dist[nx][ny] = cost;
-                        if (dir == curDir) {
-                            dq.offerFirst(new int[]{nx, ny});
-                        } else {
-                            dq.offerLast(new int[]{nx, ny});
-                        }
-                    }
+        int[][] direction={ {0,1}, {0,-1}, {1,0}, {-1,0} };
+
+        while(!dque.isEmpty()){
+            int[] arr=dque.pollFirst();
+            int r=arr[0];
+            int c=arr[1];
+
+            //Visited adjacent cells
+            for(int i=0; i<4; i++){
+                int nr=r+direction[i][0];
+                int nc=c+direction[i][1];
+                int cost=(grid[r][c]!=(i+1))?1:0;
+
+                if( nr>=0 && nr<m && nc>=0 && nc<n && minCost[r][c]+cost<minCost[nr][nc]){
+                    minCost[nr][nc]=minCost[r][c]+cost;
+
+                    if(cost==1){
+                        dque.offerLast(new int[]{nr,nc});
+                    }else{
+                        dque.offerFirst(new int[]{nr,nc});
+                    }                   
                 }
             }
         }
-        return dist[m - 1][n - 1];
+
+        return minCost[m-1][n-1];
     }
 }
