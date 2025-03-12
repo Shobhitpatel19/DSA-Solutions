dcf84aa61aa95c2a3f92bfa40231e01b6b902d28
Shobhitpatel19 <95868005+Shobhitpatel19@users.noreply.github.com>
2025-03-12 22:27:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = str.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            int res = sln.minCostClimbingStairs(arr);
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//Back-end complete function Template for Java
+
+class Solution {
+    static int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        if (n == 2) {
+            return Math.min(cost[0], cost[1]);
+        }
+        
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
+        for (int i = 2; i < n; i++) {
+            dp[i] = cost[i] + Math.min(dp[i-1], dp[i-2]);
+        }
+        
+        // The minimum cost to reach the top is the minimum of the last two steps
+        return Math.min(dp[n-1], dp[n-2]);
+    }
+}
+
